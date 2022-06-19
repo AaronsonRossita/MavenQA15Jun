84135278f76190eceb7d4b21bb637abff651f819
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2022-06-19 17:10:39 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        String s = "abc";
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+}
